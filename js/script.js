98fcbe49b1f05f1f492596ec1581a0e7e41a3ed1
M_Mek <mmekraz781@gmail.com>
2023-11-17 16:18:36 -0500

    completed step 1

--- js/script.js
+++ js/script.js
@@ -14,3 +14,29 @@ const remainingGuessCountSpan = document.querySelector(".remaining span");
 const guessMessage = document.querySelector(".message");
 // Hidden by default, will appear when game is finished
 const playAgainButton = document.querySelector(".play-again");
+// Dummy word for guessing game
+const word = "magnolia";
+
+// Call the function and pass it the word variable as the argument.
+// You should see 8 ● symbols on the screen, one for each letter in the word “magnolia.”
+// Hint: You’ll need to use an array and then join it back to a string using the .join("") method.
+const wordProgressUpdater = function (word) {
+  const array = [];
+  for (let letter of word) {
+    console.log(letter);
+    array.push("●");
+  }
+  wordInProgress.innerText = array.join("");
+};
+
+wordProgressUpdater(word);
+
+// Because you’re working with a form, you want to prevent the default behavior of clicking a button,
+// the form submitting, and then reloading the page. To prevent this reloading behavior,
+// add this line of code at the top of the callback function: e.preventDefault();.
+guessButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const guess = guessInput.value;
+  console.log(guess);
+  guessInput.value = "";
+});
